3c17ff8c4b7f8662fc9caba51e9a8b1d413c010b
andrewlee <ochiailee64@gmail.com>
2016-10-09 15:16:12 -0700

    this has add/subtract

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,7 +8,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button one, two, add, enter;
+    // see, we dont need a calculator class is we want this to work, sure it might be more efficient, but we dont need it.
+    //also, instead of doing the anonymous inner-class, it may be easier to just do a normal listener as we dont have
+    // to write a new class every single time
+    private Button one, two, three, four, five, add, subtract, enter;
     int sign;//0 for add, 1 for subtract, 2 for multiply, 3 for divide,
     private TextView test;
     private String[] numbers; //this is where all the numbers are stored
@@ -44,6 +47,28 @@ public class MainActivity extends AppCompatActivity {
                 test.setText(numbers[holder]);
             }
         });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numbers[holder]+=3;
+                test.setText(numbers[holder]);
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numbers[holder]+=4;
+                test.setText(numbers[holder]);
+
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numbers[holder]+=5;
+                test.setText(numbers[holder]);
+            }
+        });
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,6 +81,18 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder++;
+                sign =2;
+                test.setText("0");
+                numbers[holder] = "0";
+
+            }
+            
+        });
+
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,6 +103,10 @@ public class MainActivity extends AppCompatActivity {
                     int third = first + second;
                     test.setText(third+"");
                 }
+                else if(sign ==2) {
+                    int third = first - second;
+                    test.setText(third + "");
+                }
             }
         });
     }
@@ -74,8 +115,12 @@ public class MainActivity extends AppCompatActivity {
         one = (Button) findViewById(R.id.button_one);
         two = (Button) findViewById(R.id.button_two);
         add = (Button) findViewById(R.id.button_add);
+        subtract = (Button) findViewById(R.id.button_subtract);
         enter = (Button) findViewById(R.id.button_enter);
         test = (TextView) findViewById(R.id.textView);
+        three = (Button) findViewById(R.id.button_three);
+        four = (Button) findViewById(R.id.button_four);
+        five = (Button) findViewById(R.id.button_five);
 
     }
 }
